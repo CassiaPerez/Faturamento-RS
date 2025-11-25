712a2bd45ad8d25237db1b03dae7d94d18cfe17a
CassiaPerez <cassia.perez@grupocropfield.com.br>
2025-11-25 16:32:39 -0300

    Added base44Client.js

---
+++ src/api/entities.js
@@ -0,0 +1,17 @@
+import { base44 } from './base44Client';
+
+
+export const Pedido = base44.entities.Pedido;
+
+export const SolicitacaoFaturamento = base44.entities.SolicitacaoFaturamento;
+
+export const HistoricoFaturamento = base44.entities.HistoricoFaturamento;
+
+export const LogSincronizacao = base44.entities.LogSincronizacao;
+
+export const SnapshotSincronizacao = base44.entities.SnapshotSincronizacao;
+
+
+
+// auth sdk:
+export const User = base44.auth;
